145f3b6842fbd201419e8a5723f8ea80ebf87c0e
Tomas Fuerl <tomas.fuerl@schumacher-packaging.com>
2019-12-19 18:06:40 +0100

    Code eingekürzt

--- model.js
+++ model.js
@@ -3,17 +3,9 @@
 /* returned das aktuelle Datum in Form von DD.MM.JJJJ aus */
 function date_heute_ddmmjjjj() {
 	var today = new Date();
-	var dd = today.getDate();
-	var mm = today.getMonth() + 1; //January is 0!
+	var dd = fuehrendeNull(today.getDate());
+	var mm = fuehrendeNull(today.getMonth() + 1); //January is 0!
 	var yyyy = today.getFullYear();
-
-	if (dd < 10) {
-		dd = '0' + dd
-	}
-
-	if (mm < 10) {
-		mm = '0' + mm
-	}
 
 	today = dd + '.' + mm + '.' + yyyy;
 	return today;
