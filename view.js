3a17614c75e2c0877026b8ca21a7aad60f25385b
Tomas Fuerl <tomas.fuerl@schumacher-packaging.com>
2019-12-19 18:10:18 +0100

    Quellcodedoku

--- view.js
+++ view.js
@@ -3,15 +3,20 @@
 /* Wandelt einen Zeitpunkt von ms seit 1970 in eine Uhrzeit vom Format HH:MM (negative Zeiträume werden auch gehandelt)*/
 function millisecondsToHHMM(int_milliseconds) {
 	var minus = false;
-	var ms_pro_tag = 24 /* Stunden */ * 60 /* Minuten */ * 60 /* Sekunden */ * 1000 /* Millisekunden */;
+    var ms_pro_tag = 24 /* Stunden */ * 60 /* Minuten */ * 60 /* Sekunden */ * 1000 /* Millisekunden */;
+    
+    /* Kürzt Tage ein */
 	int_milliseconds %= ms_pro_tag; 
 
+    /* merkt sich, Zahl negativ war und macht sie Positiv */
 	if (int_milliseconds < 0) {
 		minus = true;
 		int_milliseconds = 0 - Number(int_milliseconds);
-	}
+    }
+    /* Wandeelt in Format HH:MM um */
 	int_milliseconds = fuehrendeNull(Math.floor(int_milliseconds / (60 * 60 * 1000)) + ":" + fuehrendeNull(Math.floor(int_milliseconds - (Math.floor(int_milliseconds / (60 * 1000))))));
-	if (minus == true) {
+    /* fügt bei Bedarf ein Minus vorne an */
+    if (minus == true) {
 		int_milliseconds = "-" + int_milliseconds;
 	}
 	return int_milliseconds;
