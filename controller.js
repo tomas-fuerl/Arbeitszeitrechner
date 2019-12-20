8946fe87d41d87b8b9e6e30e1d06836fc8c1b0a2
Tomas Fuerl <tomas.fuerl@schumacher-packaging.com>
2019-12-20 18:56:35 +0100

    Kontroll-Logik implementiert mit zykl. Aufrufen

--- controller.js
+++ controller.js
@@ -1,1 +1,27 @@
-/* Hier wird die Berechnungslogik hinterlegt */
+/* Hier wird die Kontroll-Logik hinterlegt */
+
+/* Funktion wird geladen und kümmert sich um das Initiieren aller Funktionen */
+function onloadfunction() {
+    /* lade alle set-Funktionen*/
+    felder_aktualisieren();
+
+    /* Zyklische Aufrufe */
+    window.setInterval(set_bis_jetzt, 1000);
+    window.setInterval(set_uhren, 1000);
+
+    /* Wenn Felder geändert werden, aktualisiere sie */
+    document.getElementById("start").addEventListener("oninput", felder_aktualisieren);
+    document.getElementById("ueberstunden").addEventListener("oninput", felder_aktualisieren);
+    document.getElementById("fruehstueckspause").addEventListener("oninput", felder_aktualisieren);
+    document.getElementById("mittagspause").addEventListener("oninput", felder_aktualisieren);
+}
+
+function felder_aktualisieren() {
+    /* lade alle set-Funktionen*/
+    set_regulaere_Arbeitszeit();
+    set_regulaeres_Ende();
+    set_fruehestes_Ende();
+    set_bis_jetzt();
+    set_ueberstunden();
+    set_uhren();
+}
