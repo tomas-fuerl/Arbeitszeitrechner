e74818a17a43a0379a30970fa9276df7aa3a1e37
Tomas Fuerl <tomas.fuerl@schumacher-packaging.com>
2019-12-20 16:49:40 +0100

    Model an date.js angepasst

--- model.js
+++ model.js
@@ -1,23 +1,17 @@
-/* Hier werden alle Funktionen hinterlegt, die sich Daten holt aus der Umgebung */
+/* Hier werden alle Funktionen hinterlegt, die sich Daten holen aus der Umgebung */
 
-/* returned das aktuelle Datum in Form von DD.MM.JJJJ aus */
-function date_heute_ddmmjjjj() {
-	var today = new Date();
-	var dd = fuehrendeNull(today.getDate());
-	var mm = fuehrendeNull(today.getMonth() + 1); //January is 0!
-	var yyyy = today.getFullYear();
-
-	today = dd + '.' + mm + '.' + yyyy;
-	return today;
+/* gibt den Arbeitsbeginn in date.js-Format aus anhand vom Input-Feld "start"*/
+function datetime_Arbeitsbeginn_ausgeben() {
+    var uhrzeit = document.getElementById("start").value;
+    return Date.today().at(uhrzeit);
 }
 
-/* Diese Funktion nimmt einen dezimalen Zeitraum in Stunden als float und rechnet diesen mit dem darinstehenden Array zusammen zu einem Integer-Zeitraum */
-function int_ueberstunden_in_MS() {
+/* nimmt den Wert aus dem Feld "ueberstunden" als float und rechnet diesen mit dem darinstehenden Array zusammen (Ausgabe als Dezimalstunde)*/
+function float_ueberstunden() {
 
     /*  Hier kann man alle Ueberstunden eintragen, die von einzelnen Monaten sind
         Dabei gilt der Syntax "Zahl, Zahl, Zahl, Zahl, ...". Diese Liste ist unendlich lang erweiterbar. 
-        Achtung! Das amerikanisches Zahlenformat gilt.  */
-
+        Achtung! Das amerikanisches Zahlenformat gilt (Punkt statt Komma). */
     var ueberstunden = [
         /*Jan*/	0,
         /*Feb*/	0,
@@ -33,85 +27,60 @@ function int_ueberstunden_in_MS() {
         /*Dez*/	0
     ];
 
-    var gesamtsaldo = 0;
     /* Zusammenrechnen des Arrays */
+    var gesamtsaldo = 0;
     for (var i = 0; i < ueberstunden.length; i++) {
         gesamtsaldo += ueberstunden[i];
     }
     gesamtsaldo += document.getElementById("ueberstunden").value;
-    gesamtsaldo = decH_zu_intMS(gesamtsaldo);
     return gesamtsaldo;
 }
-
-/* normalisiert den Uhrzeit-Input-String in Form von HH:MM von HTML zu Millisekunden seit 1.1.1970 */
-function arbeitsbeginn(str_arbeitsbeginn_Oberflaeche) {
-    var stunden = document.getElementById("start").value.substring(0, 2);
-    var minuten = document.getElementById("start").value.substring(3);
-
-    /* Wandle in Minuten um und dann in Sekunden und Millisekunden */
-    int_arbeitsbeginn_in_MS = (stunden * 60 + minuten) * 60 * 1000;
-
-    /* Wandle aktuelles Datum in Millisekunden um */
-    var date_today = Date.parse(date_heute_ddmmjjjj());
-
-    /* rechne heutiges Datum mit Uhrzeit zusammen */
-    int_arbeitsbeginn_in_MS += date_today;
-
-    return int_arbeitsbeginn_in_MS;
-}
-
-/* gibt die Reguläre Arbeitszeit in ms aus */
-function int_arbeitszeit_in_MS() {
+/* gibt die Reguläre Arbeitszeit in Dezimalstunden aus */
+function float_Arbeitszeit_decH() {
     /* 8 Stunden Arbeit und 6 Minuten Wegezeit (0.10 Stunden)*/
     var arbeitszeit = 8.10;
-    return decH_zu_intMS(arbeitszeit);
+    return arbeitszeit;
 }
 
-/* gibt aktuellen Zeitpunkt in ms aus */
-function int_timestamp_in_MS() {
-    var today = new Date();
-    return today.getTime();
+/* gibt aktuellen Zeitpunkt aus */
+function datetime_timestamp() {
+    return Date();
 }
 
-/* gibt Pausenzeiten anhand von checkboxen in MS zurück*/
-function int_pausen_in_MS() {
-    var int_pausenzeit_in_MS = 0;
-    /* Wenn Frühstückpausenauswahlfelt angehakt ist += 15 min */
+/* gibt Pausenzeiten anhand von checkboxen in Dezimalstunden zurück*/
+function float_pausen_decH() {
+    var float_pausenzeit = 0;
+    /* Wenn Frühstückspausenauswahlfeld angehakt ist += 15 min */
     if (document.getElementById("fruehstueckspause").checked) {
-        int_pausenzeit_in_MS += 0.25;
+        float_pausenzeit += 0.25;
     }
-    /* Wenn Frühstückpausenauswahlfelt angehakt ist += 15 min */
+    /* Wenn Mittagspausenauswahlfeld angehakt ist += 30 min */
     if (document.getElementById("mittagspause").checked) {
-        int_pausenzeit_in_MS += 0.5;
+        float_pausenzeit += 0.5;
     }
-    return decH_zu_intMS(int_pausenzeit_in_MS);
+    return float_pausenzeit;
 }
 
-/* gibt Zeitraum seit Arbeitsbeginn in ms aus */
-function int_bis_jetzt_in_MS() {
-    var bis_jetzt = int_timestamp_in_MS() - arbeitsbeginn() - int_pausen_in_MS();
+/* gibt Zeitraum seit Arbeitsbeginn in Dezimalstunden aus; berücksichtigt Pausenseit */
+function float_bis_jetzt_decH() {
+    var bis_jetzt = datetime_Arbeitsbeginn_ausgeben().getElapsed();
+    /* Wandle in decH um */
+    bis_jetzt = bis_jetzt/ (1000*60*60);
+    bis_jetzt -= float_pausen_decH();
     return bis_jetzt;
 }
 
-/* gibt Zeitpunkt oder Zeitraum in Texas aus in MS */
-function int_texastime_in_MS(int_deutschlandtime_in_MS) {
-    int_deutschlandtime_in_MS -= 7 /* Stunden */ * 60 /* Minuten */ * 60 /* Sekunden */ * 1000 /* Millisekunden */;
-    /* fange negative Werte ab und gebe stattdessen eine gültige Zeit am Vortag an */
-    if (int_deutschlandtime_in_MS < 0) {
-        int_deutschlandtime_in_MS += 24 /* Stunden */ * 60 /* Minuten */ * 60 /* Sekunden */ * 1000 /* Millisekunden */;
-    }
-    return int_deutschlandtime_in_MS;
+/* gibt einen Zeitpunkt in Texas-Zeit aus (-7 Stunden) */
+function datetime_texastime(datetime_deutschlandtime) {
+    return datetime_deutschlandtime.add(-7).hours();
 }
 
 /* gibt Regulären Endzeitpunkt aus (Arbeitsbeginn + Pausen + Arbeitszeit) */
-function int_regulaeres_Ende_in_MS() {
-    var int_regulaeres_Ende = arbeitsbeginn() + int_pausen_in_MS() + int_arbeitszeit_in_MS();
-    return int_regulaeres_Ende;
+function datetime_regulaeres_Ende() {
+    return datetime_Arbeitsbeginn_ausgeben().add(float_pausen_decH()+float_Arbeitszeit_decH()).hours();;
 }
 
 /* gibt den Frühesten Endzeitpunkt aus (Regulärer Endzeitpunkt - Überstunden) */
 function int_fruehestes_Ende_in_MS() {
-    var int_fruehestes_Ende = int_regulaeres_Ende_in_MS() - int_ueberstunden_in_MS();
-    return int_fruehestes_Ende;
+    return  datetime_regulaeres_Ende().add(-float_ueberstunden()).hours();
 }
-
