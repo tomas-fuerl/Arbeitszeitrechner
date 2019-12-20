c2e336718aa35ddd6a79a57ca7f3504baa0a5227
Tomas Fuerl <tomas.fuerl@schumacher-packaging.com>
2019-12-20 19:22:04 +0100

    Aktualisierung jetzt richtig bei Eingabe

--- controller.js
+++ controller.js
@@ -1,21 +1,5 @@
 /* Hier wird die Kontroll-Logik hinterlegt */
 
-/* Funktion wird geladen und kümmert sich um das Initiieren aller Funktionen */
-function onloadfunction() {
-    /* lade alle set-Funktionen*/
-    felder_aktualisieren();
-
-    /* Zyklische Aufrufe */
-    window.setInterval(set_bis_jetzt, 1000);
-    window.setInterval(set_uhren, 1000);
-
-    /* Wenn Felder geändert werden, aktualisiere sie */
-    document.getElementById("start").addEventListener("oninput", felder_aktualisieren);
-    document.getElementById("ueberstunden").addEventListener("oninput", felder_aktualisieren);
-    document.getElementById("fruehstueckspause").addEventListener("oninput", felder_aktualisieren);
-    document.getElementById("mittagspause").addEventListener("oninput", felder_aktualisieren);
-}
-
 function felder_aktualisieren() {
     /* lade alle set-Funktionen*/
     set_regulaere_Arbeitszeit();
@@ -24,4 +8,14 @@ function felder_aktualisieren() {
     set_bis_jetzt();
     set_ueberstunden();
     set_uhren();
+}
+
+/* Funktion wird geladen und kümmert sich um das Initiieren aller Funktionen */
+function onloadfunction() {
+    /* lade alle set-Funktionen*/
+    felder_aktualisieren();
+
+    /* Zyklische Aufrufe */
+    window.setInterval(set_bis_jetzt, 1000);
+    window.setInterval(set_uhren, 1000);
 }
