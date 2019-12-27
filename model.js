da84634ed2ca47fdfeb85e4d74a5c14ebbec33fc
Tomas Fuerl <tomas.fuerl@schumacher-packaging.com>
2019-12-27 10:08:24 +0100

    funktion float_ueberstunden in eigene Datei

--- model.js
+++ model.js
@@ -11,35 +11,6 @@ function datetime_Arbeitsbeginn() {
     }
 }
 
-/* nimmt den Wert aus dem Feld "ueberstunden" als float und rechnet diesen mit dem darinstehenden Array zusammen (Ausgabe als Dezimalstunde)*/
-function float_ueberstunden() {
-
-    /*  Hier kann man alle Ueberstunden eintragen, die von einzelnen Monaten sind
-        Dabei gilt der Syntax "Zahl, Zahl, Zahl, Zahl, ...". Diese Liste ist unendlich lang erweiterbar. 
-        Achtung! Das amerikanisches Zahlenformat gilt (Punkt statt Komma). */
-    var ueberstunden = [
-        /*Jan*/	0,
-        /*Feb*/	0,
-        /*Mär*/	0,
-        /*Apr*/	0,
-        /*Mai*/	0,
-        /*Jun*/	0,
-        /*Jul*/	0,
-        /*Aug*/	0,
-        /*Sep*/	0,
-        /*Okt*/	0,
-        /*Nov*/	0,
-        /*Dez*/	0
-    ];
-
-    /* Zusammenrechnen des Arrays */
-    var gesamtsaldo = 0;
-    for (var i = 0; i < ueberstunden.length; i++) {
-        gesamtsaldo += ueberstunden[i];
-    }
-    gesamtsaldo += document.getElementById("ueberstunden").value;
-    return parseFloat(gesamtsaldo);
-}
 /* gibt die Reguläre Arbeitszeit in Dezimalstunden aus */
 function float_Arbeitszeit_decH() {
     /* 8 Stunden Arbeit und 6 Minuten Wegezeit (0.10 Stunden)*/
